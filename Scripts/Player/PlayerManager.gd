cd29073ef0ae2946a25fde0babaf25aef4b59d50
Arthur de Vasconcelos Vidal <58999675+ArthurVasconcelosVidal@users.noreply.github.com>
2022-12-14 16:18:22 -0300

    Creating a direction using camera, player position and the input

--- Scripts/Player/PlayerManager.gd
+++ Scripts/Player/PlayerManager.gd
@@ -6,7 +6,9 @@ extends Node
 # var b = "text"
 onready var inputNode = get_node("InputSystem")
 onready var movementNode = get_node("MovementBehaviour")
-
+onready var meshNode = get_node("MeshNode")
+onready var cameraNode = get_node("Camera")
+onready var debugDrawNode = get_node("DrawDebugNode") 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
--- Scripts/Player/PlayerMovement.gd
+++ Scripts/Player/PlayerMovement.gd
@@ -4,7 +4,9 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var testao = 2 
+var movementSpeed = 5
+var rotationSpeed = 3
+
 onready var playerManager = get_parent()
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,9 +15,25 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#print(playerManager.inputNode.stickValue)
-	#print(playerManager.inputNode.sitckMagnitude)
-	pass
+	MovementBehaviour()
+
+
+func MovementBehaviour():
+	var finalDir = ObjectRelatedDirection(playerManager.inputNode.stickValue, playerManager.cameraNode) 
+	playerManager.debugDrawNode.draw_line_3d(playerManager.translation, finalDir.normalized() * 50, Color(1, 1, 0))
+	print(finalDir.normalized())
+
+func ObjectRelatedDirection(var inputDirection, var objectTransform):
+	var forwardDirection = ProjectOnPlane(-playerManager.cameraNode.get_global_transform().basis.z, playerManager.get_transform().basis.y)
+	var rightDirection = ProjectOnPlane(playerManager.cameraNode.get_global_transform().basis.x, playerManager.get_transform().basis.y)
+	var finalDirection = forwardDirection * inputDirection.y + rightDirection * inputDirection.x
+	return finalDirection
+
+
+func ProjectOnPlane(var point, var planeNormal):
+	var normPlane = Plane(planeNormal,0)
+	var projectedPoint = normPlane.project(point)
+	return projectedPoint  
 
 func _on_InputSystem_SouthButton_Pressed():
 	print("Q Pressed")
